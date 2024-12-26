671937bcf37e936f5e901caefee273c769f73c68
franciscovaz <ftomasvaz@gmail.com>
2024-12-26 23:35:47 +0000

    - add global styles;

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,14 @@
 import { ThemeProvider } from 'styled-components';
+
 import { defaultTheme } from './styles/themes/default';
+import { GlobalStyle } from './styles/global';
+
 export function App() {
   return (
     <ThemeProvider theme={defaultTheme}>
       <p>Testing themes with styled components</p>
+
+      <GlobalStyle />
     </ThemeProvider>
   )
 }
