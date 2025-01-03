48f6c28d58092dc3756884d4617f5f09a63c1a6a
franciscovaz <ftomasvaz@gmail.com>
2025-01-03 15:34:16 +0000

    - add theme background;

--- src/layouts/DefaultLayout/styles.ts
+++ src/layouts/DefaultLayout/styles.ts
@@ -4,4 +4,7 @@ export const LayoutContainer = styled.div`
   max-width: 74rem; /* 74 * 16px */
   height: calc(100vh - 10rem);
   margin: 5rem auto;
+
+  padding: 2.5rem;
+  background: ${(props) => props.theme['gray-800']};
 `
