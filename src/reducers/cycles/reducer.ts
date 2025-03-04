2b644dc2acc7114c7f272083bf9910cb83f6d92b
franciscovaz <ftomasvaz@gmail.com>
2025-03-04 10:33:40 +0000

    - imeer to interrupt current cycle;

--- src/reducers/cycles/reducer.ts
+++ src/reducers/cycles/reducer.ts
@@ -22,18 +22,20 @@ export function CyclesReducer(state: CyclesState, action: any) {
         draft.cycles.push(action.payload.newCycle),
           (draft.activeCycleId = action.payload.newCycle.id)
       })
-    case ActionTypes.INTERRUPT_CURRENT_CYCLE:
-      return {
-        ...state,
-        cycles: state.cycles.map((cycle) => {
-          if (cycle.id === state.activeCycleId) {
-            return { ...cycle, stopDate: new Date() }
-          } else {
-            return cycle
-          }
-        }),
-        activeCycleId: null,
+    case ActionTypes.INTERRUPT_CURRENT_CYCLE: {
+      const currentCycleIndex = state.cycles.findIndex((cycle) => {
+        return cycle.id === state.activeCycleId
+      })
+
+      if (currentCycleIndex < 0) {
+        return state
       }
+
+      return produce(state, (draft) => {
+        draft.activeCycleId = null
+        draft.cycles[currentCycleIndex].stopDate = new Date()
+      })
+    }
     case ActionTypes.MARK_CURRENT_CYCLE_AS_FINISHED:
       return {
         ...state,
